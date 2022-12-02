77b2a40e325a1a04cd05f6d68ee3fd2e3c107d4c
sanqdinh <69653759+sanqdinh@users.noreply.github.com>
2022-12-02 11:24:06 -0500

    fix JAX compatability

--- Experimental/Feasibility_tests_bank.py
+++ Experimental/Feasibility_tests_bank.py
@@ -5,7 +5,7 @@ Created on Fri Dec  2 10:48:37 2022
 @author: sqd0001
 """
 import jax.numpy as jnp
-
+import numpy as np
 # %% Feasibility Test number 1
 '''
 In this example, the feasible regions is 9*(u[0] - 3)^2 + 2.5 - u[1] >= 0
@@ -16,10 +16,10 @@ def FeasTest1_implicit(u,y):
     return jnp.array([LHS0, LHS1])
 
 def FeasTest1(u):
-    y = jnp.zeros(2)
+    y = np.zeros(2)
     y[0] = jnp.sqrt(9*(u[0] - 3)**2 + 2.5 - u[1])
     y[1] = (u[0] + u[1]**2)
-    return jnp.array(y)
+    return jnp.array([y])
 
 AIS_bound = jnp.array([[0.0, 6.0],
                     [0.0, 6.0]])
