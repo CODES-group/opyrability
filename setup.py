27a619a11f7a01172fa4af928262661a55db7412
Victor Alves <50674829+victoraalves@users.noreply.github.com>
2023-02-28 19:54:21 -0500

    Begin working on pyproject.toml update.
    -

--- setup.py
+++ setup.py
@@ -5,10 +5,13 @@ package_dir = \
 {'': 'src'}
 
 modules = \
-['pyprop', 'operability_grid_mapping', 'PolyhedraVolAprox']
+['pyprop', 'PolyhedraVolAprox']
 install_requires = \
-['numpy>=1.24.1,<2.0.0',
- 'polytope>=0.2.3,<0.3.0',
+['cvxopt>=1.3.0,<2.0.0',
+ 'cyipopt @ git+https://github.com/mechmotum/cyipopt.git@master',
+ 'matplotlib>=3.6.2,<4.0.0',
+ 'numpy>=1.24.1,<2.0.0',
+ 'polytope @ git+https://github.com/tulip-control/polytope.git@main',
  'scipy>=1.10.0,<2.0.0',
  'tqdm>=4.64.1,<5.0.0']
 
@@ -25,8 +28,9 @@ setup_kwargs = {
     'package_dir': package_dir,
     'py_modules': modules,
     'install_requires': install_requires,
-    'python_requires': '>=3.9,<4.0',
+    'python_requires': '>=3.9.0,<4.0.0',
 }
 
 
 setup(**setup_kwargs)
+
