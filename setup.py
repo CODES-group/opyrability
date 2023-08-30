d359a766f7f7beb5b8cd706c7765ceeddf31424b
Victor <victor_alves@live.com>
2023-08-29 23:39:03 -0400

    version bump to be compatible with conda.

--- setup.py
+++ setup.py
@@ -17,9 +17,8 @@ install_requires = \
 
 setup_kwargs = {
     'name': 'opyrability',
-    'version': '1.2',
+    'version': '1.2.1',
     'description': 'Process operability analysis in Python',
-    'long_description': "# opyrability - Process Operability Analysis in Python.\n\nWelcome to opyrability, a Python-based package for performing [Process Operability](https://www.sciencedirect.com/science/article/pii/S1474667017338028) analysis.\n\nopyrability is developed by the Control, Optimization and Design for Energy and Sustainability (CODES) Group at West Virginia University under Dr. [Fernando V. Lima](https://fernandolima.faculty.wvu.edu/)'s supervision.\n\n![](/docs/opyrability_overview.png)\n\nAuthors:\n[Victor Alves](https://github.com/victoraalves) and [San Dinh](https://github.com/sanqdinh)\n\n\n## Installation\n\nFrom PyPI:\n\n```console\npip install opyrability\n```\n\nThen install [Cyipopt](https://github.com/mechmotum/cyipopt) (non-automated dependency) and [Polytope's latest version hosted on GitHub](https://github.com/tulip-control/polytope):\n\n```console\nconda install -c conda-forge cyipopt\n```\n\n```\npip install git+https://github.com/tulip-control/polytope.git@main\n```\n\nConda packaging coming soon.\n\n## Documentation and Process Operability Principles\n\nFull documentation and discussion regarding process operability principles are available in [opyrability's online portal.](https://codes-group.github.io/opyrability/)\n\n\n\n",
     'author': 'Victor Alves',
     'author_email': 'None',
     'maintainer': 'None',
