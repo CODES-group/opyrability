2f12dee932714abf1c97a24a11210c8f6df2ad8b
Victor <victor_alves@live.com>
2023-08-31 17:41:02 -0400

    1.4.0 release.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = \
 
 setup_kwargs = {
     'name': 'opyrability',
-    'version': '1.3.9',
+    'version': '1.4.0',
     'description': 'Process operability analysis in Python',
     'author': 'Victor Alves',
     'author_email': 'None',
