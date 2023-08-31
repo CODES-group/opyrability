ef54c136964151ae4beca17d0cce21d6b34acba8
Victor Alves <50674829+victoraalves@users.noreply.github.com>
2023-08-31 15:31:10 -0400

    python build 3.9 onwards

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = \
 
 setup_kwargs = {
     'name': 'opyrability',
-    'version': '1.3.6',
+    'version': '1.3.7',
     'description': 'Process operability analysis in Python',
     'author': 'Victor Alves',
     'author_email': 'None',
