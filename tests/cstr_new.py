da0be9be31c35a9ce6cb0ec963dca7ef65504960
Victor <victor_alves@live.com>
2023-10-10 23:28:20 -0400

    continuation of cstr2 model

--- tests/cstr_new.py
+++ tests/cstr_new.py
@@ -1,25 +1,25 @@
 import jax.numpy as np
-from scipy.optimize import root
+from scipy.optimize import root, root_scalar
 import matplotlib.pyplot as plt
 from opyrability import AIS2AOS_map
-from jax import jacrev
+from jax import jacrev, grad, jit, jacfwd, hessian
 
 from jax.config import config
 config.update("jax_enable_x64", True)
 
 
 # Values from Ray #
-beta = 0.35
-xi = 1.00
-gamma = 20.00
-phi = 0.11
-sigma_s = 0.44
-sigma_b = 0.06
-q0 = 1.00
-qc = 1.00
-x10 = 1.00
-x20 = 0.00
-x30 = -0.05
+# beta = 0.35
+# xi = 1.00
+# gamma = 20.00
+# phi = 0.11
+# sigma_s = 0.44
+# sigma_b = 0.06
+# q0 = 1.00
+# qc = 1.00
+# x10 = 1.00
+# x20 = 0.00
+# x30 = -0.05
 
 
 
@@ -54,12 +54,16 @@ dH1 = 20000
 dH2 = 20000
 eta1 = E1/E2
 eta2 = dH1/dH2
+Tr  = 599.67
+Tc0 = 529.67
+
+x40 =  (Tc0 - Tr)/ Tr
 
 phi2 = (Ab * np.exp(-gamma*(eta1 - 1 ))) / Aa
 
 
 
-
+@jit
 # Define cstr function
 def cstr(x, AIS):
     
@@ -84,14 +88,40 @@ def cstr2(x, AIS):
     term1 = q0*(x30 - x)
     term2 = phi*fx3*AIS[1]*beta
     term3 = (q0*x10)/(q0 + phi*fx3*AIS[1])
-    term4 = 
+    term4 = (eta2*phi2*(fx3**(eta1-1)))/(q0 + phi*phi2*(fx3**(eta1))*AIS[1])
+    term5 = (q0*x20 + (phi*fx3*AIS[1]*q0*x10)/(q0 + phi*fx3*AIS[1]))
+    term6 = (sigma_s*AIS[1] + sigma_b)*qc*AIS[0]*(x - x40)
+    term7 = (qc*AIS[0] + (sigma_s*AIS[1] +  sigma_b)*xi)
+    
+    g = term1 + term2*(term3 + (term4*term5)) + (term6/term7)
+    
+    return g
+
+
 
 
 jac_cstr1_raw=jacrev(cstr)
+hess_cstr_raw = jacfwd(jacrev(cstr2))
 
+grad_cstr2_raw =  jacrev(cstr2)
+
+hess_cstr2_raw = jacfwd(jacrev(cstr2))
+
+
+
+@jit
 def jac_cstr(x, *args):
     return np.array(jac_cstr1_raw(x, *args))
 
+@jit
+def hess_cstr(x, *args):
+    return np.array(hess_cstr_raw(x, *args))
+@jit
+def grad_cstr2(x, *args):
+    return np.array(grad_cstr2_raw(x, *args))
+@jit
+def hess_cstr2(x, *args):
+    return np.array(hess_cstr2_raw(x, *args))
 # Define m function
 # def m(u):
     
@@ -147,6 +177,42 @@ def m(u):
         jacket_temp = np.nan
 
     return np.array([Temp_dimensionless, conversion]).reshape(2,)
+
+
+def m_cstr2(u):
+    solution = root_scalar(cstr2, x0=0.1, args=u, method='halley',
+                           fprime=grad_cstr2, fprime2 = hess_cstr2,
+                           bracket= (0, 1))
+    
+    
+    Temp_dimensionless = solution.root
+    Temp = Tr*(1 + Temp_dimensionless)
+    fx1 = np.exp((gamma*Temp_dimensionless)/(1 + Temp_dimensionless))
+    xA_dimensionless = ((q0*x10) / (q0 + phi*fx1*u[1]))
+    conversion = 1 - (xA_dimensionless / x10)
+    # jacket_temp = (qc*u[0]*x30 + xi*(sigma_s*u[1] + sigma_b)*xA_dimensionless) / \
+    #     (qc*u[0] + xi*(sigma_s*u[1] + sigma_b))
+        
+    return np.array([conversion, Temp]).reshape(2,)
+        
+
+def m_cstr22(u):
+    x0=np.array([0.1])
+    solver = NonlinearSystemSolver([cstr2],u)
+    # x0 = np.array([0.5, 0.5])
+    solution = solver.solve(x0)
+    
+    
+    Temp_dimensionless = solution
+    Temp = Tr*(1 + Temp_dimensionless)
+    fx1 = np.exp((gamma*Temp_dimensionless)/(1 + Temp_dimensionless))
+    xA_dimensionless = ((q0*x10) / (q0 + phi*fx1*u[1]))
+    conversion = 1 - (xA_dimensionless / x10)
+    # jacket_temp = (qc*u[0]*x30 + xi*(sigma_s*u[1] + sigma_b)*xA_dimensionless) / \
+    #     (qc*u[0] + xi*(sigma_s*u[1] + sigma_b))
+        
+    return np.array([conversion, Temp]).reshape(2,)
+    
 
 def generate_edge_points(bounds, n_points):
     """
@@ -253,56 +319,70 @@ def generate_edge_points(bounds, n_points):
 # plt.title("Conversion vs. Temperature (Dimensionless)")
 # plt.show()
 
-# import cyipopt
+import cyipopt
 
 
-# class NonlinearSystemSolver:
-#     def __init__(self, funcs, args):
-#         self.funcs = funcs
-#         self.args = args
-#         # Compute the Jacobian of the vector function using jacrev
-#         self.jac_func = jacrev(self.vector_func)
+class NonlinearSystemSolver:
+    def __init__(self, funcs, args):
+        self.funcs = funcs
+        self.args = args
+        # Compute the Jacobian of the vector function using jacrev
+        self.jac_func = jacrev(self.vector_func)
+        self.hess_func = hessian(self.objective)
     
-#     def vector_func(self, x):
-#         """Vector function representing the system of equations."""
-#         return np.array([f(x, self.args) for f in self.funcs])
+    def vector_func(self, x):
+        """Vector function representing the system of equations."""
+        return np.array([f(x, self.args) for f in self.funcs])
     
-#     def objective(self, x):
-#         """Objective function: sum of squares of residuals."""
-#         residuals = np.array([f(x, self.args) for f in self.funcs])
-#         return np.sum(residuals**2)
+    def objective(self, x):
+        """Objective function: sum of squares of residuals."""
+        residuals = np.array([f(x, self.args) for f in self.funcs])
+        return np.sum(residuals**2)
     
-#     def gradient(self, x):
-#         """Calculate gradient using the Jacobian."""
-#         # Evaluate the Jacobian at the current x
-#         J = self.jac_func(x)
-#         # Compute the gradient as J^T * f(x)
-#         return J.T @ self.vector_func(x)
+    def gradient(self, x):
+        """Calculate gradient using the Jacobian."""
+        # Evaluate the Jacobian at the current x
+        J = self.jac_func(x)
+        # Compute the gradient as J^T * f(x)
+        return J.T @ self.vector_func(x)
     
-#     def solve(self, x0):
-#         """Solve the system using IPOPT."""
-#         n = len(x0)
-#         x_L = np.ones(n) * -1.0e19
-#         x_U = np.ones(n) * 1.0e19
-#         problem = cyipopt.Problem(n=n, m=0, problem_obj=self, lb=x_L, ub=x_U)
-#         x, info = problem.solve(x0)
-#         return x
+    def hessian(self, x, lagrange, obj_factor):
+        """Calculate Hessian of the objective function."""
+        # For systems of equations without constraints, the Hessian of the Lagrangian 
+        # is just obj_factor times the Hessian of the objective.
+        H = self.hess_func(x)
+        return obj_factor * H
+    
+    def solve(self, x0):
+        """Solve the system using IPOPT."""
+        n = len(x0)
+        x_L = np.ones(n) * -1.0e5
+        x_U = np.ones(n) * 1.0e5
+        problem = cyipopt.Problem(n=n, m=0, problem_obj=self, lb=x_L, ub=x_U)
+        problem.addOption('print_level', 0)
+        x, info = problem.solve(x0)
+        return x
 
-# def m2(u):
-#     x0=np.array([0.1])
-#     solver = NonlinearSystemSolver([cstr],u)
-#     # x0 = np.array([0.5, 0.5])
-#     solution = solver.solve(x0)
-   
+def m2(u):
+    x0=np.array([0.1])
+    solver = NonlinearSystemSolver([cstr],u)
+    # x0 = np.array([0.5, 0.5])
+    solution = solver.solve(x0)
+    # solution = root_scalar(cstr, x0=0.1, args=u, method='newton',
+    #                        fprime=jac_cstr, fprime2 = hess_cstr,
+    #                        bracket= [0, 1.00], maxiter=5000, xtol=1e-10,
+    #                        rtol= 1e-11)
     
-#     Temp_dimensionless = solution
-#     fx1 = np.exp((gamma*Temp_dimensionless)/(1 + Temp_dimensionless))
-#     xA_dimensionless = ((q0*x10) / (q0 + phi*fx1*u[1]))
-#     conversion = 1 - (xA_dimensionless / x10)
-#     jacket_temp = (qc*u[0]*x30 + xi*(sigma_s*u[1] + sigma_b)*xA_dimensionless)/ \
-#         (qc*u[0] +  xi*(sigma_s*u[1] + sigma_b))
+    # print(solution.converged)
+    
+    Temp_dimensionless = solution
+    fx1 = np.exp((gamma*Temp_dimensionless)/(1 + Temp_dimensionless))
+    xA_dimensionless = ((q0*x10) / (q0 + phi*fx1*u[1]))
+    conversion = 1 - (xA_dimensionless / x10)
+    jacket_temp = (qc*u[0]*x30 + xi*(sigma_s*u[1] + sigma_b)*xA_dimensionless)/ \
+        (qc*u[0] +  xi*(sigma_s*u[1] + sigma_b))
         
-#     return np.array([Temp_dimensionless, conversion]).reshape(2,)
+    return np.array([Temp_dimensionless, conversion]).reshape(2,)
         
         
     
@@ -311,11 +391,54 @@ def generate_edge_points(bounds, n_points):
 
 
 
-# AIS_bound =  np.array([[0.25,  0.75],
-#                         [0.25, 1.00]])
+# AIS_bound =  np.array([[0.00,  0.75],
+#                        [0.00,  1.00]])
 
-# AIS_resolution = [100, 100]
-# AOS, AIS = AIS2AOS_map(m, AIS_bound, AIS_resolution)
+# AIS_resolution = [20, 20]
+# AOS, AIS = AIS2AOS_map(m2, AIS_bound, AIS_resolution)
+
+AIS_bound2 =  np.array([[0.8,   16.00],
+                        [0.2,   6.00]])
+
+AIS_resolution2 = [5, 5]
+AOS, AIS = AIS2AOS_map(m_cstr22, AIS_bound2, AIS_resolution2)
+
+
+# # Run the model
+# Temps = []
+# Conversions = []
+
+# AIS_bound =  np.array([[0.80, 4.00],
+#                         [0.2, 8.00]])
+
+# n_points = 100
+
+# u_values = generate_edge_points(AIS_bound, n_points)
+
+
+# for u in u_values:
+#     result = m_cstr22(u)
+#     Temps.append(result[0])
+#     Conversions.append(result[1])
+
+
+# # Plotting
+# plt.figure(figsize=(10, 5))
+# plt.scatter(u_values[:,0], u_values[:,1], marker='o', color='b', linestyle='-')
+# plt.xlabel("Normalized Coolant flow")
+# plt.ylabel("Normalized Volume")
+# plt.grid(True)
+# plt.title("Normalized Coolant flow vs. Normalized Volume")
+# plt.show()
+
+# # Plotting
+# plt.figure(figsize=(10, 5))
+# plt.scatter(Conversions,Temps, marker='o', color='b', linestyle='-')
+# plt.xlabel("Temperature (Dimensionless)")
+# plt.ylabel("Conversion")
+# plt.grid(True)
+# plt.title("Conversion vs. Temperature (Dimensionless)")
+# plt.show()
 
 # AIS = AIS.reshape(100,-1)
 
